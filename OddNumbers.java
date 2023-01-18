a65479297c4339efc0d7c3ccd65c6f5610f0d48a
ROMIT PAL <85341342+rysfgu@users.noreply.github.com>
2023-01-18 19:17:50 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The odd numbers are:");
+        for (int i=1;i<=100;i++){
+            if(i%2!=0){
+                System.out.println(i );
+            }
+        }
+    }
+}
